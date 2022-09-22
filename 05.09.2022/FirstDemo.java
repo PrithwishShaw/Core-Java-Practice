823cb6734f0b7056107c643056d0f79819269aef
PrithwishShaw <113018508+PrithwishShaw@users.noreply.github.com>
2022-09-22 09:22:48 +0530

    Add files via upload

---
+++ 05.09.2022/FirstDemo.java
@@ -0,0 +1,17 @@
+/*
+Program:This is the first java program
+@author:Prithwish Shaw
+@Date: 5sept2022
+*/
+
+//declaring class
+class FirstDemo
+{
+	//calling main method
+	public static void main(String args[])
+	{
+	System.out.println("Hello World"); //printing hello world
+	}
+	//end of main 
+}
+//end of class
