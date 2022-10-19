c92bda9aee35064085decb0767bb681558955da7
Prithwish Shaw <113018508+PrithwishShaw@users.noreply.github.com>
2022-10-19 12:33:16 +0530

    Add files via upload

---
+++ Shopping Cart Project/Item.java
@@ -0,0 +1,58 @@
+/*
+Program: Bean class for Items 
+@author: Prithwish Shaw
+@Date: 23 Sept 2022
+*/
+
+
+class Item
+{
+	//instance variable
+	private String name;
+	private int qty;
+	private double totalPrice;
+	
+	Item()// no arg constructor
+	{
+		
+	}
+	
+	//paramaterized constructor
+	Item(String name, int qty, double unitPrice)
+	{
+		this.name=name;
+		this.qty=qty;
+		this.totalPrice= qty* unitPrice;
+	}
+	
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public void setQty(int qty)
+	{
+		this.qty=qty;
+	}
+	
+	public int getQty()
+	{
+		return this.qty;
+	}
+	
+	public void setTotalPrice(double totalPrice)
+	{
+		this.totalPrice=totalPrice;
+	}
+	
+	public double getTotalPrice()
+	{
+		return this.totalPrice;
+	}
+	
+}
